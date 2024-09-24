c811642ec86548f870299376a4fced40a0eb5456
Ahmed Allam <3lam.ahmedd@gmail.com>
2024-09-24 19:37:48 +0300

    toolbar

--- app/src/main/java/com/example/rwadfive/MainActivity.kt
+++ app/src/main/java/com/example/rwadfive/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.rwadfive
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.SeekBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -25,56 +28,25 @@ class MainActivity : AppCompatActivity() {
         var selectedWeight = 0
         binding.name = "RWAD 5"
 
-        binding.maleBtn.setOnClickListener() {
-            binding.maleBtn.setBackgroundColor(Color.parseColor("#2196F3"))
-            binding.femaleBtn.setBackgroundColor(Color.parseColor("#797878"))
-            selectedGender = Gender.Male
-        }
+        setSupportActionBar(binding.toolbar)
 
-        binding.femaleBtn.setOnClickListener() {
-            binding.femaleBtn.setBackgroundColor(Color.parseColor("#2196F3"))
-            binding.maleBtn.setBackgroundColor(Color.parseColor("#797878"))
-            selectedGender = Gender.Female
-        }
+    }
 
-        binding.heightSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-                binding.heightTextView.text = "Height: $progress cm"
-                selectedheight = progress
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_settings ->{
+                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show()
             }
-
-            override fun onStartTrackingTouch(seekBar: SeekBar) {}
-            override fun onStopTrackingTouch(seekBar: SeekBar) {}
-        })
-
-        binding.weightSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-                binding.weightTextView.text = "Weight: $progress kg"
-                selectedWeight = progress
+            R.id.action_profile ->{
+                Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show()
             }
-
-            override fun onStartTrackingTouch(seekBar: SeekBar) {}
-            override fun onStopTrackingTouch(seekBar: SeekBar) {}
-        })
-
-        binding.calcBtn.setOnClickListener() {
-
-            val bmi = selectedWeight / (selectedheight / 100 * selectedheight / 100)
-            val result = when (bmi.toDouble()) {
-                in 0.0..18.5 -> "Underweight"
-                in 18.5..24.9 -> "Normal"
-                in 25.0..29.9 -> "Overweight"
-                in 30.0..34.9 -> "Obesity Class 1"
-                in 35.0..39.9 -> "Obesity Class 2"
-                else -> "Obesity Class 3"
-            }
-
-            AlertDialog.Builder(this).setTitle("Result")
-                .setMessage("Your BMI is $bmi and you are $result").setPositiveButton("OK", null)
-                .show()
         }
-
-
+        return super.onOptionsItemSelected(item)
     }
 
 
