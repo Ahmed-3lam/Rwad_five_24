013c0b613200400527b51fe1f32f26659fb3e5a7
Ahmed Allam <3lam.ahmedd@gmail.com>
2024-09-24 18:44:14 +0300

    bmi calc

--- app/src/main/java/com/example/rwadfive/MainActivity.kt
+++ app/src/main/java/com/example/rwadfive/MainActivity.kt
@@ -1,44 +1,85 @@
 package com.example.rwadfive
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.SeekBar
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+
+import com.example.rwadfive.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val rollBtn:Button = findViewById(R.id.rollBtn)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        var selectedGender = Gender.Male
+        var selectedheight = 0
+        var selectedWeight = 0
+        binding.name = "RWAD 5"
 
-        rollBtn.setOnClickListener(){
-            rollDice()
+        binding.maleBtn.setOnClickListener() {
+            binding.maleBtn.setBackgroundColor(Color.parseColor("#2196F3"))
+            binding.femaleBtn.setBackgroundColor(Color.parseColor("#797878"))
+            selectedGender = Gender.Male
+        }
+
+        binding.femaleBtn.setOnClickListener() {
+            binding.femaleBtn.setBackgroundColor(Color.parseColor("#2196F3"))
+            binding.maleBtn.setBackgroundColor(Color.parseColor("#797878"))
+            selectedGender = Gender.Female
+        }
+
+        binding.heightSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                binding.heightTextView.text = "Height: $progress cm"
+                selectedheight = progress
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+        })
+
+        binding.weightSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                binding.weightTextView.text = "Weight: $progress kg"
+                selectedWeight = progress
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+        })
+
+        binding.calcBtn.setOnClickListener() {
+
+            val bmi = selectedWeight / (selectedheight / 100 * selectedheight / 100)
+            val result = when (bmi.toDouble()) {
+                in 0.0..18.5 -> "Underweight"
+                in 18.5..24.9 -> "Normal"
+                in 25.0..29.9 -> "Overweight"
+                in 30.0..34.9 -> "Obesity Class 1"
+                in 35.0..39.9 -> "Obesity Class 2"
+                else -> "Obesity Class 3"
+            }
+
+            AlertDialog.Builder(this).setTitle("Result")
+                .setMessage("Your BMI is $bmi and you are $result").setPositiveButton("OK", null)
+                .show()
         }
 
 
-
-
-
-    }
-    private fun rollDice(){
-        val diceImage:ImageView = findViewById(R.id.dice_image)
-
-        val randomInt = (1..6).random()
-      val imageResource = when(randomInt){
-          1 -> R.drawable.dice_1
-          2 -> R.drawable.dice_2
-          3 -> R.drawable.dice_3
-          4 -> R.drawable.dice_4
-          5 -> R.drawable.dice_5
-          else -> R.drawable.dice_6
-      }
-        diceImage.setImageResource(imageResource)
     }
 
+
+}
+
+enum class Gender {
+    Male, Female
 }
